3916a9bd4a7956db45f83a3d0debb8ffbcf75c7c
prophet satin <prophetsatin@gmail.com>
2024-01-26 15:03:57 +0300

    first com

---
+++ MagicCalculator/ViewController.swift
@@ -0,0 +1,16 @@
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Time to calculate")
+
+    }
+    
+
+
+}
+
